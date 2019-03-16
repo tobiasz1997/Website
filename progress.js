37b76c10208773a3f2773d1e7b9b8f020def78d4
Grzegorz <grzegorz.tobiasz97@gmail.com>
2019-03-16 14:20:06 +0100

    istill working

---
+++ progress.js
@@ -0,0 +1,15 @@
+$(function(){
+    for(let i=0; i<9; i++){
+        let $bla = $('progress').eq(i).attr('data-values');
+        $('progress').eq(i).on('mousemove', function(){
+            $(this).attr('value', '1');
+            $(this).animate({
+                value: $bla
+            },600,'swing');
+        }); 
+        let text = '<span>'+ $bla +'%</span>';
+        $('progress').eq(i).after(text);
+    }
+    $('span').hide().delay(300).fadeIn(800);
+});
+
